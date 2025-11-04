f0d5be7f7635e1e93378c35cf5239f2809ef16b4
flaviaglenda <flaviaglenda15@gmail.com>
2025-11-04 14:30:36 -0300

    login

--- screens/RealizarLogin.js
+++ screens/RealizarLogin.js
@@ -9,6 +9,7 @@ import {
   KeyboardAvoidingView,
   Platform,
   ActivityIndicator,
+  Image,
 } from "react-native";
 import { supabase } from "../supabase";
 
@@ -68,7 +69,6 @@ export default function RealizarLogin({ navigation }) {
         return;
       }
 
-      // Navega para a tela do professor
       navigation.navigate("TelaProfessor", {
         userId: user.id,
         professorId: profData.id,
@@ -88,11 +88,18 @@ export default function RealizarLogin({ navigation }) {
       style={{ flex: 1 }}
     >
       <View style={styles.container}>
-        <Text style={styles.title}>Login do Professor</Text>
+        <Image
+          source={require("../src/assets/logo_escola.png")}
+          style={styles.logo}
+        />
+
+        <Text style={styles.title}>Bem-vindo(a)</Text>
+        <Text style={styles.subtitle}>Faça login para continuar</Text>
 
         <TextInput
           style={styles.input}
           placeholder="E-mail"
+          placeholderTextColor="#888"
           keyboardType="email-address"
           value={email}
           onChangeText={setEmail}
@@ -101,6 +108,7 @@ export default function RealizarLogin({ navigation }) {
         <TextInput
           style={styles.input}
           placeholder="Senha"
+          placeholderTextColor="#888"
           secureTextEntry
           value={senha}
           onChangeText={setSenha}
@@ -123,10 +131,7 @@ export default function RealizarLogin({ navigation }) {
           style={styles.linkContainer}
         >
           <Text style={styles.linkText}>
-            Não tem conta?{" "}
-            <Text style={{ color: "#20568c", fontWeight: "bold" }}>
-              Cadastre-se
-            </Text>
+            Não tem conta? <Text style={styles.linkBold}>Cadastre-se</Text>
           </Text>
         </TouchableOpacity>
       </View>
@@ -138,40 +143,69 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    padding: 20,
-    backgroundColor: "#fff",
+    padding: 30,
+    backgroundColor: "#c3cdd5ff",
+  },
+  logo: {
+    width: 250,
+    height: 250, 
+    alignSelf: "center",
+    marginBottom:-10,
+    marginTop: -80,
   },
   title: {
-    fontSize: 24,
+    fontSize: 32,
     fontWeight: "bold",
-    marginBottom: 30,
+    marginBottom: 5,
     textAlign: "center",
     color: "#20568c",
   },
-  input: {
-    backgroundColor: "#f2f2f2",
-    marginBottom: 12,
-    padding: 12,
-    borderRadius: 10,
+  subtitle: {
     fontSize: 16,
+    color: "#3e3e3eff",
+    textAlign: "center",
+    marginBottom: 30,
+  },
+  input: {
+    backgroundColor: "#ecececff",
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 15,
+    fontSize: 16,
+    color: "#000",
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 3 },
   },
   button: {
     backgroundColor: "#20568c",
     padding: 15,
-    borderRadius: 10,
+    borderRadius: 20,
     alignItems: "center",
+    shadowColor: "rgba(17, 69, 80, 1)",
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 5 },
+    marginTop: 10,
+    width:110,
+    marginLeft: 120,
   },
   buttonText: {
     color: "#fff",
     fontWeight: "bold",
-    fontSize: 16,
+    fontSize: 18,
   },
   linkContainer: {
-    marginTop: 15,
+    marginTop: 20,
     alignItems: "center",
   },
   linkText: {
-    fontSize: 15,
-    color: "#333",
+    fontSize: 16,
+    color: "#000",
+  },
+  linkBold: {
+    color: "#20568c",
+    fontWeight: "bold",
   },
 });
