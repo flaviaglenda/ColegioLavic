0dc16c78a305ca224c12ab71e1a5616d76ece6ef
flaviaglenda <flaviaglenda15@gmail.com>
2025-11-04 09:01:25 -0300

    criaçao cadastro

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import RealizarLogin from './screens/RealizarLogin';
+import RealizarCadastro from './screens/CadastroProfessor';
 import CadastrarTurma from './screens/CadastrarTurma';
-import ListarTurma from './screens/ListarTurma';
+import ListarTurmas from './screens/ListarTurmas';
 import TelaProfessor from './screens/TelaProfessor';
+import AtividadesTurma from './screens/AtividadesTurma';
+
 
 const Drawer = createDrawerNavigator();
-function DrawerNavigator() {
-return (
-<NavigationContainer>
-<Drawer.Navigator>
-<Drawer.Screen name="Login" component={RealizarLogin} />
-<Drawer.Screen name="Cadastrar turma" component={CadastrarTurma} />
-<Drawer.Screen name="Turmas" component={ListarTurma} />
-<Drawer.Screen name="Gerenciamento professor" component={TelaProfessor} />
-<Drawer.Screen name="Atividades turmas" component={AtividadesTurma} />
-</Drawer.Navigator>
-</NavigationContainer>
-);
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerShown: true, 
+        }}
+      >
+        <Drawer.Screen name="Login" component={RealizarLogin} />
+        <Drawer.Screen name="Cadastrar turma" component={CadastrarTurma} />
+        <Drawer.Screen name="Turmas" component={ListarTurmas} />
+        <Drawer.Screen
+          name="Gerenciamento professor"
+          component={TelaProfessor}
+        />
+        <Drawer.Screen name="Atividades Turma" component={AtividadesTurma} />
+         <Drawer.Screen name="Cadastro" component={RealizarCadastro} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
