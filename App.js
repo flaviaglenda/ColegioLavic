ea0d97da703dd014c520ef08613aacb0691025ee
flaviaglenda <flaviaglenda15@gmail.com>
2025-11-04 08:45:05 -0300

    criação do app e telas

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import RealizarLogin from './screens/RealizarLogin';
+import CadastrarTurma from './screens/CadastrarTurma';
+import ListarTurma from './screens/ListarTurma';
+import TelaProfessor from './screens/TelaProfessor';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Drawer = createDrawerNavigator();
+function DrawerNavigator() {
+return (
+<NavigationContainer>
+<Drawer.Navigator>
+<Drawer.Screen name="Login" component={RealizarLogin} />
+<Drawer.Screen name="Cadastrar turma" component={CadastrarTurma} />
+<Drawer.Screen name="Turmas" component={ListarTurma} />
+<Drawer.Screen name="Gerenciamento professor" component={TelaProfessor} />
+<Drawer.Screen name="Atividades turmas" component={AtividadesTurma} />
+</Drawer.Navigator>
+</NavigationContainer>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
